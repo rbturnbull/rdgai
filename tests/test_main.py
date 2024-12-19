4bd8e30a9599501161da82e34514029cb7c111b7
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-12-19 15:02:03 +1100

    :cop: all cli code covered in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -97,3 +97,54 @@ def test_main_import_classifications_csv(tmp_path):
     assert '<relation active="1" passive="3" ana="#category3" resp="#ground_truth"/>' in output_text
     assert '<relation active="2" passive="3" ana="#category2" resp="#ground_truth"/>' in output_text
 
+
+def test_main_evaluate(tmp_path):
+    report = tmp_path / "report.html"
+    confusion_matrix = tmp_path / "confusion_matrix.csv"
+    confusion_matrix_plot = tmp_path / "confusion_matrix.svg"
+
+    result = runner.invoke(app, [
+        "evaluate", str(TEST_DATA_DIR/"minimal_output.xml"), str(TEST_DATA_DIR/"ground_truth.xml"), 
+        "--report", str(report),
+        "--confusion-matrix", str(confusion_matrix),
+        "--confusion-matrix-plot", str(confusion_matrix_plot),
+    ])
+    assert result.exit_code == 0
+
+    out = result.stdout
+    assert "No rdgai relations found in predicted document" not in out
+    assert "recall 33" in out
+    assert "f1 33" in out
+    assert "accuracy 50" in out
+
+    assert report.exists()
+    report_text = report.read_text()
+    assert '"y":["category1","category2","category3"]' in report_text
+    assert '<p class="card-text small">Ground Truth</p>' in report_text
+
+    assert confusion_matrix.exists()
+    confusion_matrix_text = confusion_matrix.read_text()
+    assert ',category1,category2,category3' in confusion_matrix_text
+    assert 'category1,1,0,0' in confusion_matrix_text
+    assert 'category2,0,0,0' in confusion_matrix_text
+    assert 'category3,0,1,0' in confusion_matrix_text
+
+    assert confusion_matrix_plot.exists()
+
+
+@patch("rdgai.apparatus.Doc.flask_app")
+def test_main_serve(mock_flask_app, tmp_path):
+    output = tmp_path / "output"
+
+    # Mock the run method in the Flask app
+    mock_run = mock_flask_app.return_value.run
+
+    # Simulate calling the CLI command
+    result = runner.invoke(
+        app, 
+        ["serve", str(TEST_DATA_DIR/"minimal.xml"), str(output), "--debug", "--all-apps"]
+    )
+    assert result.exit_code == 0
+
+    # Check if the Flask app's `run` method was called with the correct arguments
+    mock_run.assert_called_once_with(debug=True, use_reloader=False)
